4cebabdc0a5c14ead57c7f2346a185d79523e213
prabinb50 <joshiprabin17@gmail.com>
2024-08-08 18:01:58 +0545

    commit

---
+++ questionsOfVariables&DataTypes.js
@@ -0,0 +1,8 @@
+// 1) Create a const object called ''product'' to store information in the picture.
+const product = {
+    name: "Parker Jotter Stabdard CT Ball Pen",
+    rating: 4,
+    price: 270,
+    offer: 10
+};
+console.log(product);
